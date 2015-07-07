c665d576d0f2baf0bb0cf3713f14a141656433ed
zergiocosta <sergio.costa@outlook.com>
2015-07-07 20:23:25 -0300

    version 1.0

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Hello Salvador
 
-For whose missing Salvador, Bahia
+WordPress plugin for whose missing Salvador, Bahia
+
+Just install and enjoy! =D
 
 ## License
 [![WTFPL](wtfpl-badge.png "WTFPL")](https://github.com/zergiocosta/hello-salvador/blob/master/LICENSE)
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  * Author: Sergio Costa
  * Author URI: http://sergiocosta.net.br/
  * Text Domain: hellosalvador
- * License: GPLv2 or later
+ * License: GPLv2
  */
 
 
@@ -15,21 +15,25 @@ function hello_salvador_background() {
 	$images = array(
 		'001.jpg',
 		'002.jpg',
-		'003.jpg'
+		'003.jpg',
+		'004.jpg',
+		'005.jpg',
+		'006.jpg',
+		'007.jpg',
+		'008.jpg'
 	); 
 	$image = $images[array_rand($images)];
 	$image = plugin_dir_url(__FILE__) . 'images/' . $image; ?>
 	<style>
-		html, body { 
+		html {
+			background-color: transparent !important;
+		} 
+		body { 
 			background: url('<?php echo $image; ?>') no-repeat center center fixed; 
-			/*background-color: rgba(255, 255, 255, 0.3);*/
 			-webkit-background-size: cover;
 			   -moz-background-size: cover;
 				 -o-background-size: cover;
 					background-size: cover;
-		}
-		#wpwrap {
-			background: rgba(0, 0, 0, 0.0);
 		}
 	</style> <?php
 }
